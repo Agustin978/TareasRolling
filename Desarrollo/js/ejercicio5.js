88078e7be72662c202b3f0d6632052d7c826ca6e
Agustin978 <loboagus10@gmail.com>
2023-02-27 09:12:59 -0300

    Realizadas las modigficaciones en la pagina para el desarrollo del ejercicio 5

--- Desarrollo/js/ejercicio5.js
+++
@@ -1,8 +0,0 @@
-let numeros = [];
-
-for(let i = 0; i<2;i++)
-{
-    numeros[i] = prompt("Introducir un valor:");
-}
-
-document.write("El resultado de la suma entre ",numeros[0]," + ",numeros[1]," es: "+(parseInt(numeros[0]) + parseInt(numeros[1])));
