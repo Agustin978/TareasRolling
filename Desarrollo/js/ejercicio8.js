ca740b28ea18a7a5dc1c98cef7a6b9aed83528b5
Agustin978 <loboagus10@gmail.com>
2023-02-27 09:41:19 -0300

    Desarrollo del ejercicio 8. Se ingresa un numero y determina si el mismo es par o impar

---
+++ Desarrollo/js/ejercicio8.js
@@ -0,0 +1,11 @@
+let numero;
+
+numero = prompt("Introducir un valor:");
+
+if(parseInt(numero) % 2 == 0)
+{
+    document.write("El numero ingresado "+numero+" es par.");
+}else
+{
+    document.write("El numero ingresado "+numero+" es impar.");
+}
